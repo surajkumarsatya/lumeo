ce5fe695731f3cc5de1cdbf0a7948b13311d1b44
Suraj <surajmail.799@gmail.com>
2025-11-12 00:10:34 +0530

    I am fine

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -89,7 +89,7 @@ export default function MainContent() {
             <h4 className="text-purple-600 uppercase tracking-widest">
               Think dexttt. Think <span className="font-bold">dexlll</span>.
             </h4>
-            <p>Hello how are you ?</p>
+            <p>I am fine ?</p>
           </div>
 
           {/* <div className="grid w-full gap-2">
